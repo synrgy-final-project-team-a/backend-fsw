fb82ac5ef7560bf3d94522a9640b7ab4b18037cc
Ilyas Mahfud <ilyasmahfud39@gmail.com>
2023-01-09 22:17:24 +0700

    middleware autentikasi

--- src/router/UserRoute.js
+++ src/router/UserRoute.js
@@ -1,7 +1,8 @@
 const {getAllUsers} = require("../controllers/UserController");
 const express = require("express");
+const { parseToken } = require("../middlewares/authorization");
 const router = express.Router();
 
-router.get("/api/users/", getAllUsers);
+router.get("/api/users/", parseToken, getAllUsers);
 
 module.exports = router;
