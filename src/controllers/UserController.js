e3056a2c2121981bf85a3fa0eea2102cbc161452
raihanpf22 <raihanpambagyo@.com>
2023-01-11 21:18:22 +0700

    merge branch detail_user, user_list

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,6 +6,8 @@ const getAllUsers = async (req, res) => {
 
   res.status(200).json(users);
 };
+
+
 const GetUserById = async (req, res) => {
   // console.log(req.user.user_name);
   const { status, message, data } = await UserService.getUserById({
