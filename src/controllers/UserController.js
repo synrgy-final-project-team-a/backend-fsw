ed48a2f48c914ca8733e270b87b73057ca797f81
Ilyas Mahfud <ilyasmahfud39@gmail.com>
2023-01-12 14:46:23 +0700

    response status and clean code

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,21 +1,15 @@
 const UserService = require("../services/UserService");
 
 const getAllUsers = async (req, res) => {
-  console.log(req.user);
   const users = await UserService.getUsers();
 
-  res.status(200).json(users);
+  res.status(users.status).json(users);
 };
 const GetUserById = async (req, res) => {
-  // console.log(req.user.user_name);
-  const { status, message, data } = await UserService.getUserById({
+  const data = await UserService.getUserById({
     email: req.user.user_name,
   });
-  res.status(status).send({
-    status: status,
-    message: message,
-    data: data,
-  });
+  res.status(data.status).json(data);
 };
 
 module.exports = { getAllUsers, GetUserById };
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -5,7 +5,7 @@ const getUsers = async () => {
   const users = await userRepository.findAllUser();
 
   return {
-    code: 200,
+    status: 200,
     message: "Retrive Data Users",
     data: users,
   };
