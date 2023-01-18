944f35a6978927ddb78cdc5c163ae733a9700cc9
raihanpf22 <raihanpambagyo@.com>
2023-01-18 23:33:27 +0700

    fixing endpoint create user for super admin role (progress(undone))

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -4,6 +4,7 @@ const UserDetails = models.user_details;
 const findAllUser = async () => {
   return await UserDetails.findAll();
 };
+
 const getUserById = async (payload) => {
   const getUser = await UserDetails.findOne({
     where: {
@@ -13,5 +14,9 @@ const getUserById = async (payload) => {
   return getUser;
 };
 
+const createProfile = async (payload) => {
+  const profile = await UserDetails.create(payload);
+  return profile;
+};
 
-module.exports = { findAllUser, getUserById };
+module.exports = { findAllUser, getUserById, createProfile };
--- src/router/UserRoute.js
+++ src/router/UserRoute.js
@@ -6,22 +6,40 @@ const {
 const express = require("express");
 const auth = require("../middlewares/authorization");
 const { validation } = require("../middlewares/validations");
-const { createUserValidation } = require("../middlewares/validations/userValidations");
+const {
+  createUserValidation,
+} = require("../middlewares/validations/userValidations");
+const multerUpload = require("../utils/multer");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "../uploads/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+});
 
 const router = express.Router();
 
 router.get(
-    "/api/users/",
-    auth.parseToken,
-    auth.checkRole(["ROLE_SUPERUSER"]),
-    getAllUsers
+  "/api/users/",
+  auth.parseToken,
+  auth.checkRole(["ROLE_SUPERUSER"]),
+  getAllUsers
 );
 router.get("/api/user/detail", auth.parseToken, GetUserById);
-router.post("/api/user/create", validation(createUserValidation),createUser);
+router.post(
+  "/api/user/create",
+  // validation(createUserValidation),
+  upload.single("avatar"),
+  createUser
+);
 
 // router.get("/api/users/", getAllUsers);
 // router.get("/api/user/detail", GetUserById);
 // router.post("/api/user/create", createUser);
-
 
 module.exports = router;
